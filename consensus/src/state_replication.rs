b14c8d9e14105998502a08d34115b29274ad7604
luoqngAwd245 <40060887+luoqngAwd245@users.noreply.github.com>
2019-07-31 22:35:28 +0800

    部分翻译提交

--- consensus/src/state_replication.rs
+++ consensus/src/state_replication.rs
@@ -22,28 +22,42 @@ use types::{
 /// of success / failure of the transactions.
 /// Note that the specific details of compute_status are opaque to StateMachineReplication,
 /// which is going to simply pass the results between StateComputer and TxnManager.
+/// 一种指定执行结果的结构。
+/// 执行负责生成新状态的ID，该状态在结果中返回。
+///
+/// 并非有效负载中的每个事务都成功：返回的向量保持事务成功/失败的布尔状态。
+/// 请注意，compute_status的具体细节对StateMachineReplication是不透明的，它只是简单地在StateComputer
+/// 和TxnManager之间传递结果。
 pub struct StateComputeResult {
     /// The new state generated after the execution.
+    /// 执行后生成的新状态。
     pub new_state_id: HashValue,
     /// The compute status (success/failure) of the given payload. The specific details are opaque
     /// for StateMachineReplication, which is merely passing it between StateComputer and
     /// TxnManager.
+    /// 给定有效负载的计算状态（成功/失败）。 StateMachineReplication的具体细节是不透明的，它只是在StateComputer和
+    /// TxnManager之间传递它。
     pub compute_status: Vec<bool>,
     /// Counts the number of `true` values in the `compute_status` field.
+     /// 计算`compute_status`字段中的'true`值的数量。
     pub num_successful_txns: u64,
     /// If set, these are the validator public keys that will be used to start the next epoch
     /// immediately after this state is committed
     /// TODO [Reconfiguration] the validators are currently ignored, no reconfiguration yet.
+    /// 如果设置，这些是验证器公钥，将在提交此状态后立即用于启动下一个纪元
     pub validators: Option<ValidatorSet>,
 }
 
 /// Retrieves and updates the status of transactions on demand (e.g., via talking with Mempool)
+/// 根据需要检索和更新交易状态（例如，通过与Mempool交谈）
 pub trait TxnManager: Send + Sync {
     type Payload;
 
     /// Brings new transactions to be applied.
     /// The `exclude_txns` list includes the transactions that are already pending in the
     /// branch of blocks consensus is trying to extend.
+    /// 带来要应用的新交易。
+    /// `exclude_txns`列表包括在协商一致试图扩展的块分支中已经挂起的事务。
     fn pull_txns(
         &self,
         max_size: u64,
@@ -52,6 +66,7 @@ pub trait TxnManager: Send + Sync {
 
     /// Notifies TxnManager about the payload of the committed block including the state compute
     /// result, which includes the specifics of what transactions succeeded and failed.
+     /// 通知TxnManager已提交块的有效负载，包括状态计算结果，其中包括事务成功和失败的详细信息。
     fn commit_txns<'a>(
         &'a self,
         txns: &Self::Payload,
@@ -87,6 +102,8 @@ impl CanonicalSerialize for ExecutedState {
 /// While Consensus is managing proposed blocks, `StateComputer` is managing the results of the
 /// (speculative) execution of their payload.
 /// StateComputer is using proposed block ids for identifying the transactions.
+/// 虽然Consensus正在管理提议的块，但`StateComputer`正在管理其有效载荷的（推测）执行结果。
+/// StateComputer正在使用建议的块ID来识别事务。
 pub trait StateComputer: Send + Sync {
     type Payload;
 
@@ -98,6 +115,8 @@ pub trait StateComputer: Send + Sync {
         // The id of a parent block, on top of which the given transactions should be executed.
         // We're going to use a special GENESIS_BLOCK_ID constant defined in crypto::hash module to
         // refer to the block id of the Genesis block, which is executed in a special way.
+        // 父块的id，应在其上执行给定的事务。
+        // 我们将使用crypto :: hash模块中定义的特殊GENESIS_BLOCK_ID常量来引用Genesis块的块id，它以特殊方式执行。
         parent_block_id: HashValue,
         // The id of a current block.
         block_id: HashValue,
@@ -106,18 +125,21 @@ pub trait StateComputer: Send + Sync {
     ) -> Pin<Box<dyn Future<Output = Result<StateComputeResult>> + Send>>;
 
     /// Send a successful commit. A future is fulfilled when the state is finalized.
+     /// 发送成功提交。 当状态最终确定时，future就会实现。
     fn commit(
         &self,
         commit: LedgerInfoWithSignatures,
     ) -> Pin<Box<dyn Future<Output = Result<()>> + Send>>;
 
     /// Synchronize to a commit that not present locally.
+    /// 同步到本地不存在的提交。
     fn sync_to(
         &self,
         commit: QuorumCert,
     ) -> Pin<Box<dyn Future<Output = Result<SyncStatus>> + Send>>;
 
     /// Get a chunk of transactions as a batch
+     /// 批量获取交易组成的块作为一个批
     fn get_chunk(
         &self,
         start_version: u64,
@@ -130,6 +152,7 @@ pub trait StateMachineReplication {
     type Payload;
     /// The function is synchronous: it returns when the state is initialized / recovered from
     /// persisted storage and all the threads have been started.
+    /// 该函数是同步的：当从持久存储初始化/恢复状态并且所有线程都已启动时，它返回。
     fn start(
         &mut self,
         txn_manager: Arc<dyn TxnManager<Payload = Self::Payload>>,
@@ -137,5 +160,6 @@ pub trait StateMachineReplication {
     ) -> Result<()>;
 
     /// Stop is synchronous: returns when all the threads are shutdown and the state is persisted.
+    /// 停止是同步的：当所有线程都关闭并且状态持久时返回。
     fn stop(&mut self);
 }
