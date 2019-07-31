fd08edc390e6c26bdee60bdabb245059f6e5fd13
luoqngAwd245 <40060887+luoqngAwd245@users.noreply.github.com>
2019-07-31 22:05:51 +0800

    提交部分翻译

--- admission_control/admission_control_service/src/lib.rs
+++ admission_control/admission_control_service/src/lib.rs
@@ -9,10 +9,16 @@
 //! AC serves two types of request from clients:
 //! 1. SubmitTransaction, to submit transaction to associated validator.
 //! 2. UpdateToLatestLedger, to query storage, e.g. account state, transaction log, and proofs.
+//!准入控制（AC）是从客户端获取公共gRPC请求的公共API端点。
+//!AC 服务两种类型来自客户端的请求
+//! 1.SubmitTransaction，提交交易到关联验证者
+//! 2.UpdateToLatestLedger，查询存储，eg 账户状态，交易日志，和证据
+//!从客户那里获取公共gRPC请求。
 
 /// Wrapper to run AC in a separate process.
+/// 运行AC在一个独立的进程中的包装
 pub mod admission_control_node;
-/// AC gRPC service.
+/// AC gRPC service. AC gRPC 服务
 pub mod admission_control_service;
 use lazy_static::lazy_static;
 use metrics::OpMetrics;
