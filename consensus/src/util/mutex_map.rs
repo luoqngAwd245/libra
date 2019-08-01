481fe565ee2aa2fbbb0cbd768d322759344f006e
luoqngAwd245 <40060887+luoqngAwd245@users.noreply.github.com>
2019-08-01 21:26:01 +0800

    部分翻译提交

--- consensus/src/util/mutex_map.rs
+++ consensus/src/util/mutex_map.rs
@@ -29,11 +29,19 @@ where
 /// MutexMap is from a 'future locks' family, meaning that lock() method does not block thread,
 /// but instead returns Future(implicitly, through 'async') that is only fulfilled when lock
 /// can be acquired.
+/// MutexMap充当互斥锁的映射，允许基于某些键进行锁定
+///
+/// 更具体地说，MutexMap :: lock（key）返回锁定保护，保证对于给定的密钥是唯一的。 直到返回的防护被删除，随后调用MutexMap :: lock同样
+/// 密钥将被阻止。
+///
+/// MutexMap来自'future locks'系列，这意味着lock（）方法不会阻塞线程，而是返回Future（隐式地，通过'async'），
+/// 只有在可以获取锁时才能实现。
 pub struct MutexMap<T>(Arc<sync::Mutex<HashMap<T, AsyncMutex>>>)
 where
     T: Hash + Eq + Clone;
 
 /// This guard is returned by MutexMap::lock, dropping this guard releases lock
+/// 这个守卫是由MutexMap :: lock返回的，放弃了这个守卫释放锁定
 pub struct MutexMapGuard<T>
 where
     T: Hash + Eq + Clone,
@@ -49,17 +57,25 @@ where
 // Operation on this map is fast, so having traditional lock does not have impact.
 //
 // Only private methods get_or_create_mutex and guard_dropped acquire lock on internal map
+// 实施概述：
+//MutexMap维护未来互斥锁的映射
+//映射本身受常规，非未来互斥锁的保护，以降低代码复杂性。
+//在这张地图上的操作很快，因此传统的锁没有影响。
+//
+//只有私有方法get_or_create_mutex和guard_dropped才能在内部地图上获取锁定
 impl<T> MutexMap<T>
 where
     T: Hash + Eq + Clone,
 {
     /// Creates new, empty MutexMap
+    /// 创建新的空MutexMap
     pub fn new() -> MutexMap<T> {
         MutexMap(Arc::new(sync::Mutex::new(HashMap::new())))
     }
 
     /// async method is fulfilled when lock can be acquired
     /// Lock is released when returned guard is dropped
+     /// 可以获取锁定时实现异步方法当退回保护时，锁定被释放
     pub async fn lock(&self, key: T) -> MutexMapGuard<T> {
         let mutex_fut = self.get_or_create_mutex(key.clone());
         let mutex_guard = mutex_fut.compat().await.unwrap();
@@ -73,6 +89,8 @@ where
 
     /// Creates new empty lock_set for given map.
     /// Use this if you want to acquire multiple locks in a loop in single task
+     /// 为给定的map创建新的空lock_set。
+    /// 如果要在单个任务中获取循环中的多个锁，请使用此选项
     pub fn new_lock_set(&self) -> LockSet<T> {
         LockSet::new(self)
     }
@@ -83,6 +101,8 @@ where
         let mutex_ref = e.or_insert_with(|| AsyncMutex::new(()));
         // This one is async lock, it will return future that keeps reference on underlining mutex
         // We can't return mutex_ref itself, because
+           // 这一个是异步锁定，它将返回保留对下划线互斥锁的引用的未来
+        // 我们不能返回mutex_ref本身，因为
         mutex_ref.lock()
     }
 
@@ -94,8 +114,14 @@ where
         // This is ok from performance point of view too:
         // removing lock is a 'happy path': if there is no pending lock,
         // then we will be able to unwrap, and in most cases won't need to re-insert mutex
+        // 这有点模糊，但我们必须先从地图中删除互斥锁，然后才能尝试打开它。 如果unwrap失败（存在待定引用），我们将mutex放回map
+        //由于我们在地图上持有锁定，因此此删除插入不会引入比赛
+        //从性能的角度来看，这也是可以的：
+        //删除锁定是一个“快乐的路径”：如果没有挂起的锁定，那么我们将能够解包，并且在大多数情况下不需要重新插入互斥锁
+
         let mutex = map.remove(&key);
         // It is possible that mutex is not in map, if multiple guards dropped concurrently
+        // 如果多个警卫同时丢弃，则互斥锁可能不在映射中
         if let Some(mutex) = mutex {
             if let Err(cloned_mutex) = mutex.try_unwrap() {
                 map.insert(key.clone(), cloned_mutex);
@@ -119,6 +145,15 @@ where
 /// Ideally we would want to track locks per task
 /// Unfortunately, currently tokio does not support TaskLocal's, so we have to have
 /// separated object to collect locks
+/// LockSet表示获取的锁定集
+///除了保持多个锁之外，LockSet还可以防止死锁
+///如果已为此锁定集获取锁定，则LockSet :: lock将失败
+///
+///如果在循环中获取多个锁，LockSet很有用，以确保没有死锁。
+///如果代码的独立部分获取锁并且不使用LockSet，则无效。
+///
+///理想情况下，我们希望跟踪每个任务的锁定
+///不幸的是，目前tokio不支持TaskLocal，所以我们必须有分离的对象来收集锁
 pub struct LockSet<'a, T>(&'a MutexMap<T>, HashMap<T, MutexMapGuard<T>>)
 where
     T: Clone + Eq + Hash;
@@ -132,6 +167,7 @@ where
     }
 
     /// Acquires lock for given object, fails if lock was already acquired through same lock set
+    /// 获取给定对象的锁定，如果已经通过相同的锁定集获取了锁定则失败
     pub async fn lock(&mut self, t: T) -> Result<(), ()> {
         if self.1.contains_key(&t) {
             return Err(());
@@ -178,9 +214,11 @@ mod test {
             events
         );
         // wait for runtime to shutdown so guard Drop releases memory in map
+        // 等待运行时关闭，以便Guard在map中释放内存
         block_on(rt1.shutdown_on_idle().compat()).unwrap();
         block_on(rt2.shutdown_on_idle().compat()).unwrap();
         // Verify that map does not hold locks in memory when they are released
+        // 验证映射在释放时不会在内存中保留锁定
         assert_eq!(0, map.held_locks_count());
     }
 
@@ -195,6 +233,7 @@ mod test {
     ) {
         // Sleep gives chance for task2 to acquire lock and fail test if lock_map does not work
         // correctly
+        // 如果lock_map无法正常工作，则睡眠为task2获取锁定和失败测试提供了机会
         thread::sleep(Duration::from_millis(1));
         map.lock(2).await;
         actions.send("Task 1 Locked 2").await.unwrap();
