50519cb6983945a541148f7de2f888f25cc01aa7
luoqngAwd245 <40060887+luoqngAwd245@users.noreply.github.com>
2019-08-01 21:47:08 +0800

    部分翻译提交

--- consensus/src/chained_bft/block_storage/block_store.rs
+++ consensus/src/chained_bft/block_storage/block_store.rs
@@ -32,6 +32,7 @@ mod block_store_test;
 
 #[derive(Debug, PartialEq)]
 /// Whether we need to do block retrieval if we want to insert a Quorum Cert.
+/// 如果我们想要插入Quorum Cert，是否需要进行块检索。
 pub enum NeedFetchResult {
     QCRoundBeforeRoot,
     QCAlreadyExist,
@@ -55,6 +56,20 @@ pub enum NeedFetchResult {
 ///             | -> C1
 ///             | -------> C2
 ///             | -------------> D3
+///
+/// 负责维护所有有效负载块和这些块的依赖性（父和先前的QC链路）。 它应该由多个线程并发访问，并且是线程安全的。
+///
+/// 基于父链接的示例树块结构。
+///                      | -> A3
+///  创世纪-> B0 -> B1 ->B2->B3
+///           | -> C1 -> C2
+///                       | -> D3
+/// QC链路的示例对应树块结构（必须遵循QC约束）。
+///                         | -> A3
+/// Genesis -> B0 -> B1 -> B2 -> B3
+///             | -> C1
+///             | -------> C2
+///             | -------------> D3
 pub struct BlockStore<T> {
     inner: Arc<RwLock<BlockTree<T>>>,
     validator_signer: ValidatorSigner<Ed25519PrivateKey>,
@@ -62,6 +77,7 @@ pub struct BlockStore<T> {
     enforce_increasing_timestamps: bool,
     /// The persistent storage backing up the in-memory data structure, every write should go
     /// through this before in-memory tree.
+    /// 持久存储备份内存数据结构，每次写入都应该在内存树之前完成。
     storage: Arc<dyn PersistentStorage<T>>,
 }
 
@@ -121,6 +137,7 @@ impl<T: Payload> BlockStore<T> {
                 version,
             };
             // if this block is certified, ensure we agree with the certified state.
+            // 如果此块已通过认证，请确保我们同意认证状态。
             if let Some(qc) = quorum_certs.get(&block.id()) {
                 assert_eq!(
                     qc.certified_state(),
@@ -156,6 +173,7 @@ impl<T: Payload> BlockStore<T> {
         let to_remove = self.inner.read().unwrap().get_all_block_id();
         if let Err(e) = self.storage.prune_tree(to_remove) {
             // it's fine to fail here, the next restart will try to clean up dangling blocks again.
+            // 在这里失败是好的，下次重启将尝试再次清理悬空块。
             error!("fail to delete block: {:?}", e);
         }
         *self.inner.write().unwrap() = tree;
@@ -173,6 +191,14 @@ impl<T: Payload> BlockStore<T> {
     /// Duplicate inserts will return the previously inserted block (
     /// note that it is considered a valid non-error case, for example, it can happen if a validator
     /// receives a certificate for a block that is currently being added).
+    ///
+    /// 如果通过所有验证测试，则执行并插入块。
+    /// 将持久保存到存储后，将Arc返回到块存储中保留的块
+    ///
+    /// 此函数假定存在祖先（否则返回MissingParent）。
+    ///
+    /// 重复插入将返回先前插入的块（请注意，它被视为有效的非错误情况，例如，如果验证器收到当前正在
+    /// 添加的块的证书，则可能会发生这种情况）。
     pub async fn execute_and_insert_block(
         &self,
         block: Block<T>,
@@ -218,6 +244,8 @@ impl<T: Payload> BlockStore<T> {
     /// Check if we're far away from this ledger info and need to sync.
     /// Returns false if we have this block in the tree or the root's round is higher than the
     /// block.
+    /// 检查我们是否远离此分类帐信息并需要同步。
+    /// 如果我们在树中有这个块或者根的圆高于块，则返回false。
     pub fn need_sync_for_quorum_cert(
         &self,
         committed_block_id: HashValue,
@@ -227,12 +255,16 @@ impl<T: Payload> BlockStore<T> {
         // the 3-chain safety rules specify that the round of the committed block must be
         // certified_block_round() - 2. In case root().round() is greater than that the committed
         // block carried by LI is older than my current commit.
+         // LedgerInfo不携带有关已提交块的轮次的信息。 但是，3链安全规则指定已提交块的循环必须为
+        // certified_block_round（） -  2.如果root（）。round（）大于LI承载的块比我当前的提交更旧。
         !(self.block_exists(committed_block_id)
             || self.root().round() + 2 >= qc.certified_block_round())
     }
 
     /// Checks if quorum certificate can be inserted in block store without RPC
     /// Returns the enum to indicate the detailed status.
+     /// 检查是否可以在没有RPC的块存储中插入仲裁证书
+    /// 返回枚举以指示详细状态。
     pub fn need_fetch_for_quorum_cert(&self, qc: &QuorumCert) -> NeedFetchResult {
         if qc.certified_block_round() < self.root().round() {
             return NeedFetchResult::QCRoundBeforeRoot;
@@ -250,9 +282,11 @@ impl<T: Payload> BlockStore<T> {
     }
 
     /// Validates quorum certificates and inserts it into block tree assuming dependencies exist.
+     /// 假设存在依赖关系，验证仲裁证书并将其插入块树中。
     pub async fn insert_single_quorum_cert(&self, qc: QuorumCert) -> Result<(), InsertError> {
         // Ensure executed state is consistent with Quorum Cert, otherwise persist the quorum's
         // state and hopefully we restart and agree with it.
+         // 确保执行状态与Quorum Cert一致，否则保持仲裁状态，并希望我们重新启动并同意它。
         let executed_state = self
             .get_state_for_block(qc.certified_block_id())
             .ok_or_else(|| InsertError::MissingParentBlock(qc.certified_block_id()))?;
@@ -283,6 +317,12 @@ impl<T: Payload> BlockStore<T> {
     /// Different execution ids are treated as different blocks (e.g., if some proposal is
     /// executed in a non-deterministic fashion due to a bug, then the votes for execution result
     /// A and the votes for execution result B are aggregated separately).
+    /// 为该块添加投票。
+    ///  返回的值包含投票结果（带有新/旧QC等）或验证错误。
+    ///  块存储不验证所投票的块是否存在于本地。
+    /// 它返回QC，如果它已形成，但不会将其插入到块存储中，因为它可能还没有所需的依赖项
+    /// 不同的执行id被视为不同的块（例如，如果由于错误而以非确定性方式执行某些提议，则执行结果A的投票和执行结果B的投票被单独聚合）。
+
     pub async fn insert_vote(
         &self,
         vote_msg: VoteMsg,
@@ -305,6 +345,16 @@ impl<T: Payload> BlockStore<T> {
     /// B_3 -> B_4, root = B_3
     ///
     /// Returns the block ids of the blocks removed.
+    ///
+    /// 将树修剪到next_root_id（保留next_root_id的块）。 任何不属于next_root_id树的分支也应该被删除。
+    ///
+    /// For example, root = B_0
+    /// B_0 -> B_1 -> B_2
+    ///         |  -> B_3 -> B4
+    /// prune_tree（B_3）应该留下
+    /// B_3 -> B_4, root = B_3
+    ///
+    /// 返回已删除块的块ID。
     pub async fn prune_tree(&self, next_root_id: HashValue) -> VecDeque<HashValue> {
         let id_to_remove = self
             .inner
@@ -318,6 +368,9 @@ impl<T: Payload> BlockStore<T> {
             // it's fine to fail here, as long as the commit succeeds, the next restart will clean
             // up dangling blocks, and we need to prune the tree to keep the root consistent with
             // executor.
+             //在这里失败是好的，只要提交成功，下一次重启就会清理
+             //悬挂块，我们需要修剪树以保持根一致
+             //执行者
             error!("fail to delete block: {:?}", e);
         }
         self.inner
@@ -329,6 +382,7 @@ impl<T: Payload> BlockStore<T> {
 
     /// If block id information is found, returns the ledger info placeholder, otherwise, return
     /// a placeholder with info of the genesis block.
+     /// 如果找到块ID信息，则返回分类帐信息占位符，否则返回带有生成块信息的占位符。
     pub fn ledger_info_placeholder(&self, id: Option<HashValue>) -> LedgerInfo {
         let block_id = match id {
             None => return Self::zero_ledger_info_placeholder(),
@@ -359,6 +413,8 @@ impl<T: Payload> BlockStore<T> {
     /// Used in case we're using a ledger info just as a placeholder for signing the votes / QCs
     /// and there is no real block committed.
     /// It's all pretty much zeroes.
+     /// 用于我们使用分类帐信息作为占位符来签署投票/ QC并且没有提交真正的块。
+    /// 这几乎都是零。
     fn zero_ledger_info_placeholder() -> LedgerInfo {
         LedgerInfo::new(
             0,
@@ -494,22 +550,27 @@ impl<T: Payload> BlockReader for BlockStore<T> {
 #[cfg(test)]
 impl<T: Payload> BlockStore<T> {
     /// Returns the number of blocks in the tree
+    /// 返回树中的块数
     fn len(&self) -> usize {
         self.inner.read().unwrap().len()
     }
 
     /// Returns the number of child links in the tree
+    /// 返回树中子链接的数量
     fn child_links(&self) -> usize {
         self.inner.read().unwrap().child_links()
     }
 
     /// The number of pruned blocks that are still available in memory
+     /// 内存中仍可用的已修剪块数
     pub(super) fn pruned_blocks_in_mem(&self) -> usize {
         self.inner.read().unwrap().pruned_blocks_in_mem()
     }
 
     /// Helper to insert vote and qc
     /// Can't be used in production, because production insertion potentially requires state sync
+     /// 帮助者插入投票和qc
+    /// 不能在生产中使用，因为生产插入可能需要状态同步
     pub async fn insert_vote_and_qc(
         &self,
         vote_msg: VoteMsg,
@@ -525,6 +586,7 @@ impl<T: Payload> BlockStore<T> {
     }
 
     /// Helper function to insert the block with the qc together
+    /// 辅助功能将qc插入块中
     pub async fn insert_block_with_qc(
         &self,
         block: Block<T>,
