ef30fdf612f05bc89c7b621d432e05b90e78c390
luoqngAwd245 <40060887+luoqngAwd245@users.noreply.github.com>
2019-08-01 21:35:43 +0800

    部分翻译提交

--- consensus/src/state_synchronizer/coordinator.rs
+++ consensus/src/state_synchronizer/coordinator.rs
@@ -21,10 +21,13 @@ use storage_client::{StorageRead, StorageReadServiceClient};
 use types::proto::transaction::TransactionListWithProof;
 
 /// unified message used for communication with Coordinator
+/// 用于与协调员通信的统一消息
 pub enum CoordinatorMsg {
     // is sent from Synchronizer to Coordinator to request a new sync
+    // 从Synchronizer发送到Coordinator以请求新的同步
     Requested(QuorumCert, oneshot::Sender<SyncStatus>),
     // is sent from Downloader to Coordinator to indicate that new batch is ready
+    // 从Downloader发送到协调员，表明新批准备就绪
     Fetched(Result<TransactionListWithProof>, QuorumCert),
 }
 
@@ -40,20 +43,29 @@ pub enum SyncStatus {
 
 /// used to coordinate synchronization process
 /// handles Consensus requests and drives sync with remote peers
+/// 用于协调同步过程
+/// 处理共识请求并驱动与远程对等方同步
 pub struct SyncCoordinator<T> {
     // communication with SyncCoordinator is done via this channel
+     // 通过此通道完成与SyncCoordinator的通信
     receiver: mpsc::UnboundedReceiver<CoordinatorMsg>,
     // connection to transaction fetcher
+     // 连接到事务提取器
     sender_to_downloader: mpsc::Sender<FetchChunkMsg>,
 
     // last committed version that validator is aware of
+    // 验证器知道的最后提交的版本
     known_version: u64,
     // target state to sync to
+    // 目标状态要同步到
     target: Option<QuorumCert>,
     // used to track progress of synchronization
+    // 用于跟踪同步进度
     sync_position: u64,
     // subscribers of synchronization
     // each of them will be notified once their target version is ready
+    // 同步的订户
+    // 一旦目标版本准备好，他们中的每一个都会收到通知
     subscribers: BTreeMap<u64, Vec<oneshot::Sender<SyncStatus>>>,
     executor_proxy: T,
 }
@@ -77,6 +89,7 @@ impl<T: ExecutorProxyTrait> SyncCoordinator<T> {
     }
 
     /// main routine. starts sync coordinator that listens for CoordinatorMsg
+    /// 主要例程。 启动侦听CoordinatorMsg的同步协调器
     pub async fn start(mut self) {
         while let Some(msg) = self.receiver.next().await {
             match msg {
@@ -102,12 +115,14 @@ impl<T: ExecutorProxyTrait> SyncCoordinator<T> {
     }
 
     /// Consensus request handler
+     /// 共识请求处理程序
     async fn handle_request(&mut self, qc: QuorumCert, subscriber: oneshot::Sender<SyncStatus>) {
         let requested_version = qc.ledger_info().ledger_info().version();
         let committed_version = self.executor_proxy.get_latest_version().await;
 
         // if requested version equals to current committed, just pass ledger info to executor
         // there might be still empty blocks between committed state and requested
+        // 如果请求的版本等于当前已提交，则只需将分类帐信息传递给执行程序，在提交状态和请求状态之间可能仍有空块
         if let Ok(version) = committed_version {
             if version == requested_version {
                 let status = match self
@@ -159,6 +174,7 @@ impl<T: ExecutorProxyTrait> SyncCoordinator<T> {
 
     /// processes batch of transactions downloaded by fetcher
     /// executes transactions, updates progress state, notifies subscribers if some sync is finished
+     /// 处理由fetcher下载的一批事务执行事务，更新进度状态，如果某些同步完成则通知订阅者
     async fn process_transactions(
         &mut self,
         txn_list_with_proof: TransactionListWithProof,
@@ -199,6 +215,7 @@ impl<T: ExecutorProxyTrait> SyncCoordinator<T> {
         };
 
         // notify subscribers if some syncs are ready
+        // 如果某些同步准备就绪，请通知订阅者
         for channels in self.subscribers.values_mut() {
             channels.drain(..).for_each(|ch| {
                 if ch.send(result.clone()).is_err() {
@@ -229,11 +246,14 @@ impl<T: ExecutorProxyTrait> SyncCoordinator<T> {
 }
 
 /// Proxy execution for state synchronization
+/// 代理执行状态同步
 pub trait ExecutorProxyTrait: Sync + Send {
     /// Return the latest known version
+     /// 返回最新的已知版本
     fn get_latest_version(&self) -> Pin<Box<dyn Future<Output = Result<u64>> + Send>>;
 
     /// Execute and commit a batch of transactions
+    /// 执行并提交一批事务
     fn execute_chunk(
         &self,
         request: ExecuteChunkRequest,
