5eada00b2cbef517a7c2fbf1af99d3b8221d4b93
luoqngAwd245 <40060887+luoqngAwd245@users.noreply.github.com>
2019-08-03 23:31:38 +0800

    部分翻译提交

--- libra_node/src/main.rs
+++ libra_node/src/main.rs
@@ -27,17 +27,20 @@ fn register_signals(term: Arc<AtomicBool>) {
         }
     }
 }
-
+/// 节点启动入口函数
 fn main() {
+    // 执行可执行文件的常见设置
     let (mut config, _logger, _args) = setup_executable(
         "Libra single node".to_string(),
         vec![ARG_PEER_ID, ARG_CONFIG_PATH, ARG_DISABLE_LOGGING],
     );
+    //设置环境，启动服务
     let (_ac_handle, _node_handle) = libra_node::main_node::setup_environment(&mut config);
 
     let term = Arc::new(AtomicBool::new(false));
+     //注册新号钩子
     register_signals(Arc::clone(&term));
-
+    
     while !term.load(Ordering::Acquire) {
         std::thread::park();
     }
